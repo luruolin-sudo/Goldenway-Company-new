aa7f2911848c27d78f63a838814e213816bd81cc
luruolin-sudo <luruolin@gwec.com.tw>
2025-12-08 10:34:25 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100);
