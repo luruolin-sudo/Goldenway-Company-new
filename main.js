411e25f0fe0f933e9b27a912619318f4ece893c0
luruolin-sudo <luruolin@gwec.com.tw>
2025-12-10 13:55:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { EXRLoader } from "./libs/EXRLoader.js"; // ✅ 載入 EXRLoader
 
 // 建立場景
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0.39, 0.39, 0.39); // ✅ 黑色背景
+scene.background = new THREE.Color(0.8, 0.8, 0.8); // ✅ 黑色背景
 
 // 建立 renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
